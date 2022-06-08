66a80c7381facc9e797dfa9d0528b8e898cdc95e
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2022-06-07 21:31:45 -0700

    Add start to main

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"log"
+)
+
+func homePage(c *gin.Context) {
+	c.String(http.StatusOK, "Home\n")
+}
+
+func uploadFile(c *gin.Context) {
+	file, err := c.FormFile("myFile")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	log.Println(file.Filename)
+
+	c.SaveUploadedFile(file, "out/uploaded-" + file.Filename)
+	c.String(http.StatusOK, fmt.Sprintf("'%s' uploaded!", file.Filename))
+}
+
+func main() {
+	router := gin.Default()
+
+	router.POST("/upload", uploadFile)
+
+	// authedSubRoute := router.Group("/api/v1/", gin.BasicAuth(gin.Accounts{
+	// 	"admin": "adminpass",
+	// }))
+
+	// authedSubRoute.GET("/", homePage)
+	// authedSubRoute.GET("/upload", uploadFile)
+	
+	listenPort := "1357"
+	router.Run(":" + listenPort)
+}
